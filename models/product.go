ed1491c9729a01748d4f40c491a16183e2b9c1fd
Sérgio Lucena <slucenadecarvalho@gmail.com>
2021-08-27 15:12:14 -0300

    conect database

--- models/product.go
+++ models/product.go
@@ -4,16 +4,17 @@ import (
 	"gorm.io/gorm"
 )
 
+
 type Product struct {
 	gorm.Model
 	PriceInCents int    `json:"price_in_cents"`
 	Title        string `json:"title"`
 	Description  string `json:"description"`
+	DiscountID   int
 	Discount     DiscountModel
 }
 type DiscountModel struct {
 	gorm.Model
-	//DiscountId          int     `json:"discount_id"  gorm:"foreignKey"`
 	Percentage   float64 `json:"percentage"`
 	ValueInCents int     `json:"value_in_cents"`
 }
